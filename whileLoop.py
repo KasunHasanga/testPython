4df86c4883ea78ae01012248605bfb69c8b32e48
[Kasun Hasanga] <[kasunhasanga96@gmail.com]>
2019-05-01 21:21:07 +0100

    Test Commit by pyCharm

---
+++ whileLoop.py
@@ -0,0 +1,16 @@
+
+i = 0
+
+while i <= 6:
+
+
+    k=6-i
+    while k <= 6:
+        print(end=" ")
+        k = k+1
+    j = i
+    while j <= 6:
+        print("* ", end="")
+        j = j + 1
+    i = i + 1
+    print("")
