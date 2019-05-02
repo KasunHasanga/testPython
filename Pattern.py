d0e2b58e447901f0e08963525136a9a68e6ea298
[Kasun Hasanga] <[kasunhasanga96@gmail.com]>
2019-05-02 20:47:33 +0100

    Done 3 pattern

---
+++ Pattern.py
@@ -0,0 +1,33 @@
+
+for i in range(6):
+    for j in range(6):
+        print("# ", end="")
+    print(" ")
+
+print(" ")
+
+
+
+
+
+y=1
+while y < 6:
+    x = y
+    while x < 6:
+        print("# ", end="")
+        x = x + 1
+    print(" ")
+    y = y + 1 
+
+print(" ")
+
+a=1
+while a<6:
+    b=6-a
+    while b<6:
+        print("# ", end="")
+        b=b+1
+    print("")
+    a=a+1
+
+
--- loopurther.py
+++ loopurther.py
@@ -2,7 +2,7 @@
 for i in range(15):
 
     if i==3:
-        continue
+        pass
 
     print(i)
 
